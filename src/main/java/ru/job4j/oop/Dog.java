be0bb6a9666d02581626582dafb870945eee4386
KirillAsmanov <mazusik1@gmail.com>
2020-03-15 18:22:40 +0300

    1.1. Создание объекта[#245083]

---
+++ src/main/java/ru/job4j/oop/Dog.java
@@ -0,0 +1,14 @@
+package ru.job4j.oop;
+/**
+ * Упр. 1.1. Создание объекта[#245083]
+ * @author Kirill Asmanov
+ * @since 015.03.2020
+ * @version 1.0.0;
+ */
+public class Dog {
+    public static void main(String[] args) {
+        Dog polkan = new Dog();
+        Dog sharik = new Dog();
+        Dog zhychka = new Dog();
+    }
+}
