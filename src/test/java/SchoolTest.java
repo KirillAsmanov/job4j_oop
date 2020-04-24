353c8f0a67ee0fa81cbf753f74f311b4392fca77
KirillAsmanov <mazusik1@gmail.com>
2020-04-24 15:31:39 +0300

    1. Фильтрация учеников.[#244873] (Тесты_исправлено)

--- src/test/java/SchoolTest.java
+++ src/test/java/SchoolTest.java
@@ -1,6 +1,6 @@
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
-import ru.job4j.oop.collections.ConvertList;
-import ru.job4j.oop.collections.fullsearch.Task;
 import ru.job4j.oop.lambda.student.School;
 import ru.job4j.oop.lambda.student.Student;
 
@@ -10,55 +10,40 @@ import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 public class SchoolTest {
-    @Test
-    public void studentsOfAClass() {
-        List<Student> studPool = new ArrayList<Student>();
-        studPool.add(new Student("A", 0));
-        studPool.add(new Student("A", 25));
-        studPool.add(new Student("A", 50));
-        studPool.add(new Student("A", 75));
-        studPool.add(new Student("A", 100));
-        List<Student> classA = School.collect(studPool, s -> (s.getScore() >= 0) && (s.getScore() < 50));
-
-        assertThat(classA.get(1).getScore(), is(25));
-    }
-
-    @Test
-    public void studentsOfBClass() {
-        List<Student> studPool = new ArrayList<Student>();
-        studPool.add(new Student("A", 0));
-        studPool.add(new Student("A", 25));
-        studPool.add(new Student("A", 50));
-        studPool.add(new Student("A", 75));
-        studPool.add(new Student("A", 100));
-        List<Student> classA = School.collect(studPool, s -> (s.getScore() >= 50) && (s.getScore() < 70));
-
-        assertThat(classA.get(0).getScore(), is(50));
-    }
-
-    @Test
-    public void studentsOfCClass() {
-        List<Student> studPool = new ArrayList<Student>();
-        studPool.add(new Student("A", 0));
-        studPool.add(new Student("A", 25));
-        studPool.add(new Student("A", 50));
-        studPool.add(new Student("A", 75));
-        studPool.add(new Student("A", 100));
-        List<Student> classA = School.collect(studPool, s -> (s.getScore() >= 70) && (s.getScore() <= 100));
-
-        assertThat(classA.get(1).getScore(), is(100));
-    }
-
-    @Test
-    public void studentsClassOnMap() {
-        List<Student> studPool = new ArrayList<Student>();
+    List<Student> studPool = new ArrayList<Student>();
+    @Before
+    public void init() {
         studPool.add(new Student("A", 0));
         studPool.add(new Student("B", 25));
         studPool.add(new Student("C", 50));
         studPool.add(new Student("D", 75));
         studPool.add(new Student("E", 100));
-        Map<String, Student> classA = School.collectToMap(studPool);
+    }
+    @After
+    public void clear() {
+        studPool.clear();
+    }
+    @Test
+    public void studentsOfAClass() {
+        List<Student> classA = School.collect(studPool, s -> (s.getScore() >= 0) && (s.getScore() < 50));
+        assertThat(classA.get(1).getScore(), is(25));
+    }
 
+    @Test
+    public void studentsOfBClass() {
+        List<Student> classA = School.collect(studPool, s -> (s.getScore() >= 50) && (s.getScore() < 70));
+        assertThat(classA.get(0).getScore(), is(50));
+    }
+
+    @Test
+    public void studentsOfCClass() {
+        List<Student> classA = School.collect(studPool, s -> (s.getScore() >= 70) && (s.getScore() <= 100));
+        assertThat(classA.get(1).getScore(), is(100));
+    }
+
+    @Test
+    public void studentsClassOnMap() {
+        Map<String, Student> classA = School.collectToMap(studPool);
         assertThat(classA.get("C"), is(new Student("C", 50)));
     }
 }
